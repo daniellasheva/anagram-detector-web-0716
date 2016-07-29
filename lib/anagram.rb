a969c3ca987895d690a0bedc318874c73bf663bc
daniellasheva <daniellasheva@gmail.com>
2016-07-29 12:36:26 -0400

    initial commit

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word=word
+  end
+
+  def match (array)
+    finalwords= []
+    array.each do |item|  #for each word in array
+      if item.chars.sort==self.word.chars.sort #if letter the same as matchletters
+          finalwords<<item    #add that word
+      end
+    end
+      finalwords
+  end
+
+end
+
